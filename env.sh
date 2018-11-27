ed62d7f5e1ac0d92cf2b8023c96d2cf9685ddeee
Austin Fox <austin.fox@qorvo.com>
2018-11-27 14:41:30 -0800

    update env.sh - should add auto knownhost??

--- env.sh
+++ env.sh
@@ -1,8 +1,3 @@
-####################### SSH KEY FOR GIT ###################################
-ssh-keygen -t rsa && ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub && eval `ssh-agent -s` && ssh-add -k
-####################### To be added to git account settings ################
-
-
 ###################### GIT PARAMETERS #####################################
 export GIT_PARENT_DIR=~
 export GIT_REPO_NAME=#your-repo
@@ -12,22 +7,28 @@ export GIT_USER=#your-gituser
 export GIT_EMAIL=#your-email
 export GITHUB_ACCESS_TOKEN=#access-token from github developer settings
 export GIT_USER_UPSTREAM=#your-user
-export GIT_ADD_ALL=False
 
+export GIT_SERVER=git@github.com 
+export GIT_HTTP=https://github.com
 
 ############################################################################
-#### DO NOT EDIT BELOW THIS LINE: derived variables
+#### Derived Variables
 ############################################################################
-
-export GIT_REMOTE_URL=git@github.com:$GIT_USER/$GIT_REPO_NAME.git
-export GIT_REMOTE_URL_HTTPS=https://github.com/$GIT_USER/$GIT_REPO_NAME.git
+export GIT_REMOTE_URL=$GIT_SERVER:$GIT_USER/$GIT_REPO_NAME.git
+export GIT_REMOTE_URL_HTTPS=$GIT_HTTP/$GIT_USER/$GIT_REPO_NAME.git
 export GIT_REMOTE_UPSTREAM=$GIT_USER_UPSTREAM/$GIT_REPO_NAME
-
 
 ####################### Git Repo where notebooks will be pushed ############
 cd $GIT_PARENT_DIR && git clone $GIT_REMOTE_URL_HTTPS
 
-
+####################### SSH KEY FOR GIT ###################################
+# add to known host ...?
+# https://serverfault.com/questions/132970/can-i-automatically-add-a-new-host-to-known-hosts
+# ssh-keygen -t rsa && ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub &&
+eval $(ssh-agent -s)
+# eval `ssh-agent`
+ssh-add /opt/jupyter/.ssh/id_rsa
+####################### To be added to git account settings ################
 
 ####################### Change in jupyter config ###########################
 if [ ! -f ~/.jupyter/jupyter_notebook_config.py ]; then
